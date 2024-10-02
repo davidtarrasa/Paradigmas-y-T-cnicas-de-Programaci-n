9f09692de5818734a5973be344e2af6e142ba6ce
David Tarrasa <202202063@alu.comillas.edu>
2024-10-02 15:16:23 +0200

    Descripción de los cambios realizados

---
+++ Taxi.cs
@@ -0,0 +1,42 @@
+﻿namespace Practice2
+{
+    class Taxi : Vehicle
+    {
+        private static string typeOfVehicle = "Taxi";
+        private bool isCarryingPassengers;
+
+        public Taxi(string plate) : base(typeOfVehicle, plate)
+        {
+            isCarryingPassengers = false;
+            SetSpeed(45.0f);
+        }
+
+        public void StartRide()
+        {
+            if (!isCarryingPassengers)
+            {
+                isCarryingPassengers = true;
+                SetSpeed(100.0f);
+                Console.WriteLine(WriteMessage("starts a ride."));
+            }
+            else
+            {
+                Console.WriteLine(WriteMessage("is already in a ride."));
+            }
+        }
+
+        public void StopRide()
+        {
+            if (isCarryingPassengers)
+            {
+                isCarryingPassengers = false;
+                SetSpeed(45.0f);
+                Console.WriteLine(WriteMessage("finishes ride."));
+            }
+            else
+            {
+                Console.WriteLine(WriteMessage("is not on a ride."));
+            }
+        }
+    }
+}
